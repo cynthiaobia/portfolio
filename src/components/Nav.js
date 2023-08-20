5b8346017c99483e95aa1967d627aa10d8e02480
Cynthia <cynthia.obia@gmail.com>
2023-08-20 15:11:28 -0500

    removed extra nav components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -20,10 +20,6 @@ export default function Nav(props) {
         <Link to='/contact'>
           <div>Contact</div>
         </Link>
-
-      {/* <NavLeft /> */}
-      {/* <NavCenter /> */}
-      {/* <NavRight /> */}
       
     </div>
   )
