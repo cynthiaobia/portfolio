78f18ecda8a9756bb970d41f9cd6b536ff8bc2a5
Cynthia <cynthia.obia@gmail.com>
2023-08-20 15:15:03 -0500

    finished content on main page

--- src/components/main/Description.js
+++ src/components/main/Description.js
@@ -4,7 +4,7 @@ export default function Description(){
     <div className="description">
       <div>
         <h2>Full Stack Developer</h2>
-        <h3>More info here</h3>
+        <h3>Explore my site to learn more about the work that I do in front end and back end development, UI/UX design, and more!</h3>
         <a href="http://linkedin.com/in/cynthiaobia" target="_blank" rel="noreferrer">LinkedIn</a>
         <a href="http://github.com/cynthiaobia" target="_blank" rel="noreferrer">GitHub</a>
       </div>
